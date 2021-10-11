850b896a29912a647cce720ecb83b9ed129079f7
ahmedmustahid <ahmed@epx.phys.tohoku.ac.jp>
2021-10-11 19:22:26 +0900

    converted Body to Stateful; Added textcontroller

--- lib/screens/messages/components/body.dart
+++ lib/screens/messages/components/body.dart
@@ -2,7 +2,7 @@
 //import 'package:chat/models/ChatMessage.dart';
 import 'package:chat/constants.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+//import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 //import 'chat_input_field.dart';
 //import 'message.dart';
@@ -12,92 +12,119 @@ class ChatMessage {
   ChatMessage({required this.messageContent, required this.messageType});
 }
 
-List<ChatMessage> messages = [
-  ChatMessage(messageContent: "Hello, Will", messageType: "receiver"),
-  ChatMessage(messageContent: "How have you been?", messageType: "receiver"),
-  ChatMessage(
-      messageContent: "Hey Kriss, I am doing fine dude. wbu?",
-      messageType: "sender"),
-  ChatMessage(messageContent: "ehhhh, doing OK.", messageType: "receiver"),
-  ChatMessage(
-      messageContent: "Is there any thing wrong?", messageType: "sender"),
-];
+class Body extends StatefulWidget {
+  @override
+  State<Body> createState() => _BodyState();
+}
 
-class Body extends StatelessWidget {
+class _BodyState extends State<Body> {
+  final TextEditingController _textEditingController = TextEditingController();
+
+  void _printLatestValue() {
+    print('text field: ${_textEditingController.text}');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Start listening to changes.
+    _textEditingController.addListener(_printLatestValue);
+  }
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is removed from the widget tree.
+    // This also removes the _printLatestValue listener.
+    _textEditingController.dispose();
+    super.dispose();
+  }
+
+  List<ChatMessage> messages = [
+    ChatMessage(messageContent: "Hello, Will", messageType: "receiver"),
+    ChatMessage(messageContent: "How have you been?", messageType: "receiver"),
+    ChatMessage(
+        messageContent: "Hey Kriss, I am doing fine dude. wbu?",
+        messageType: "sender"),
+    ChatMessage(messageContent: "ehhhh, doing OK.", messageType: "receiver"),
+    ChatMessage(
+        messageContent: "Is there any thing wrong?", messageType: "sender"),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return ProviderScope(
-      child: Stack(
-        children: <Widget>[
-          ListView.builder(
-            itemCount: messages.length,
-            shrinkWrap: true,
-            padding: EdgeInsets.only(top: 10, bottom: 10),
-            physics: NeverScrollableScrollPhysics(),
-            itemBuilder: (context, index) {
-              return Container(
-                padding:
-                    EdgeInsets.only(left: 14, right: 14, top: 10, bottom: 10),
-                child: Align(
-                  alignment: (messages[index].messageType == "receiver"
-                      ? Alignment.topLeft
-                      : Alignment.topRight),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: (messages[index].messageType == "receiver"
-                          ? Colors.grey.shade200
-                          : Colors.blue[200]),
-                    ),
-                    padding: EdgeInsets.all(16),
-                    child: Text(
-                      messages[index].messageContent,
-                      style: TextStyle(fontSize: 15),
-                    ),
+    return Stack(
+      children: <Widget>[
+        ListView.builder(
+          itemCount: messages.length,
+          shrinkWrap: true,
+          padding: EdgeInsets.only(top: 10, bottom: 10),
+          physics: NeverScrollableScrollPhysics(),
+          itemBuilder: (context, index) {
+            return Container(
+              padding:
+                  EdgeInsets.only(left: 14, right: 14, top: 10, bottom: 10),
+              child: Align(
+                alignment: (messages[index].messageType == "receiver"
+                    ? Alignment.topLeft
+                    : Alignment.topRight),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: (messages[index].messageType == "receiver"
+                        ? Colors.grey.shade200
+                        : Colors.blue[200]),
+                  ),
+                  padding: EdgeInsets.all(16),
+                  child: Text(
+                    messages[index].messageContent,
+                    style: TextStyle(fontSize: 15),
                   ),
                 ),
-              );
-            },
-          ),
-          Align(
-              alignment: Alignment.bottomLeft,
-              child: Container(
-                padding: EdgeInsets.symmetric(
-                  horizontal: kDefaultPadding,
-                  vertical: kDefaultPadding / 2,
-                ),
-                //padding: EdgeInsets.only(left: 10, bottom: 10, top: 10),
-                height: 60,
-                width: double.infinity,
-                color: Colors.white,
-                child: Row(
-                  children: <Widget>[
-                    Expanded(
-                      child: TextField(
-                        decoration: InputDecoration(
-                            hintText: "Write message...",
-                            hintStyle: TextStyle(color: Colors.black54),
-                            border: InputBorder.none),
+              ),
+            );
+          },
+        ),
+        Align(
+            alignment: Alignment.bottomLeft,
+            child: Container(
+              padding: EdgeInsets.symmetric(
+                horizontal: kDefaultPadding,
+                vertical: kDefaultPadding / 2,
+              ),
+              //padding: EdgeInsets.only(left: 10, bottom: 10, top: 10),
+              height: 60,
+              width: double.infinity,
+              color: Colors.white,
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: TextField(
+                      controller: _textEditingController,
+                      decoration: InputDecoration(
+                        hintText: "Write message...",
+                        hintStyle: TextStyle(color: Colors.black54),
+                        border: InputBorder.none,
                       ),
                     ),
-                    SizedBox(
-                      width: 15,
+                  ),
+                  SizedBox(
+                    width: 15,
+                  ),
+                  FloatingActionButton(
+                    onPressed: () {},
+                    child: Icon(
+                      Icons.send,
+                      color: Colors.white,
+                      size: 18,
                     ),
-                    FloatingActionButton(
-                      onPressed: () {},
-                      child: Icon(
-                        Icons.send,
-                        color: Colors.white,
-                        size: 18,
-                      ),
-                      backgroundColor: kPrimaryColor,
-                      elevation: 0,
-                    ),
-                  ],
-                ),
-              )),
-        ],
-      ),
+                    backgroundColor: kPrimaryColor,
+                    elevation: 0,
+                  ),
+                ],
+              ),
+            )),
+      ],
     );
   }
 }
