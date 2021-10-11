8a14a33516eb076a6fa03731b9825ae3dadc0bec
ahmedmustahid <ahmed@epx.phys.tohoku.ac.jp>
2021-10-11 16:43:44 +0900

    listview builder with messages

--- lib/screens/messages/components/body.dart
+++ lib/screens/messages/components/body.dart
@@ -6,6 +6,22 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 //import 'chat_input_field.dart';
 //import 'message.dart';
+class ChatMessage {
+  String messageContent;
+  String messageType;
+  ChatMessage({required this.messageContent, required this.messageType});
+}
+
+List<ChatMessage> messages = [
+  ChatMessage(messageContent: "Hello, Will", messageType: "receiver"),
+  ChatMessage(messageContent: "How have you been?", messageType: "receiver"),
+  ChatMessage(
+      messageContent: "Hey Kriss, I am doing fine dude. wbu?",
+      messageType: "sender"),
+  ChatMessage(messageContent: "ehhhh, doing OK.", messageType: "receiver"),
+  ChatMessage(
+      messageContent: "Is there any thing wrong?", messageType: "sender"),
+];
 
 class Body extends StatelessWidget {
   @override
@@ -13,6 +29,36 @@ class Body extends StatelessWidget {
     return ProviderScope(
       child: Stack(
         children: <Widget>[
+          ListView.builder(
+            itemCount: messages.length,
+            shrinkWrap: true,
+            padding: EdgeInsets.only(top: 10, bottom: 10),
+            physics: NeverScrollableScrollPhysics(),
+            itemBuilder: (context, index) {
+              return Container(
+                padding:
+                    EdgeInsets.only(left: 14, right: 14, top: 10, bottom: 10),
+                child: Align(
+                  alignment: (messages[index].messageType == "receiver"
+                      ? Alignment.topLeft
+                      : Alignment.topRight),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: (messages[index].messageType == "receiver"
+                          ? Colors.grey.shade200
+                          : Colors.blue[200]),
+                    ),
+                    padding: EdgeInsets.all(16),
+                    child: Text(
+                      messages[index].messageContent,
+                      style: TextStyle(fontSize: 15),
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
           Align(
               alignment: Alignment.bottomLeft,
               child: Container(
